b38d6cd38eda046491521165b96e08b453f9c08a
priyansh71 <f20201388@goa.bits-pilani.ac.in>
2021-07-03 15:19:00 +0530

    Added solution to Q5

--- Questions/question3.js
+++ Questions/question3.js
@@ -7,27 +7,20 @@ let yyyy = today.getFullYear();
 let mm = String(today.getMonth() + 1).padStart(2, "0");
 let date = dd + mm + yyyy;
 
-app.use((req, res, next) => {
+app.use("/hungry/pizza", (req, res, next) => {
   res.set({
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "*",
+    "Access-Control-Expose-Headers": "*",
+    // Expose headers makes the header sent by the server 'available' on the front-end.
   });
   next();
 });
 
 app.get("/hungry/pizza", (req, res) => {
+  let pizza = req.headers.pizza.substr(0, 5);
+  res.set({ order: date + pizza });
   res.status(200).end();
-});
-app.post("/hungry/pizza", (req, res) => {
-  let pizza = req.headers.pizza;
-  let Arr5 = [];
-  let Arrl = pizza.split("");
-  for (var i = 0; i < 5; i++) {
-    Arr5.push(Arrl[i]);
-  }
-  let pizza5 = Arr5.join("");
-  res.set("order", date + pizza5);
-  res.status(201).end();
 });
 
 app.listen(3000);
